a5a53dadf1d21f6f358e726ce9c49f2a770bbca1
Srikar <srikarg89@gmail.com>
2024-01-19 23:24:32 -0500

    Bugfix

--- src/genghis_filler/Util.java
+++ src/genghis_filler/Util.java
@@ -250,7 +250,7 @@ public class Util {
     public static boolean locIsASpawnLoc(MapLocation loc) throws GameActionException{
         // this method checks if the robot is on a spawn location
         for(MapLocation spawnCenter: robot.spawnCenters){
-            if(neumann.Util.minMovesToReach(loc, spawnCenter) <= 1){
+            if(Util.minMovesToReach(loc, spawnCenter) <= 1){
                 return true;
             }
         }
